27cf7a39fd6ecbcb8ea604f09cae02410d3b0597
Thiago Carvalho de Mello <thiago.mello@dbserver.com.br>
2023-11-22 00:09:14 -0300

    add js and ajusted css

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-const qrcode = document.getElementById("qrcode-gerado");
-
-qrcode.addEventListener("click",function(){
-    qrcode.classList.add("https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=www.teste.com");
-});
-
+function qrcode() {
+    var input = document.querySelector('input').value;
+    if (input.trim() === '') {
+        alert('Por favor, insira uma URL ou texto.');
+        return;
+    }
+    var url = "https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=" + input;
+    var img = document.getElementById('qrcode-gerado');
+    img.style.display = "block";
+    img.src = url;
+}
